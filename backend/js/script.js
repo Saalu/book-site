012fa8ce8bf1231c8981804d94072d2715e3b78c
Saalu <saaluissak@gmail.com>
2021-10-27 16:44:17 +0000

    LoginForm and BottomNav

--- backend/js/script.js
+++ backend/js/script.js
@@ -3,6 +3,15 @@ document.querySelector("#search-btn").onclick = () => {
   searchForm.classList.toggle("active");
 };
 
+let loginForm = document.querySelector(".login-form-container");
+
+document.querySelector("#login-btn").onclick = () => {
+  loginForm.classList.toggle("active");
+};
+document.querySelector("#close-login-btn").onclick = () => {
+  loginForm.classList.remove("active");
+};
+
 window.onscroll = () => {
   searchForm.classList.remove("active");
 
@@ -15,9 +24,9 @@ window.onscroll = () => {
 
 window.onload = () => {
   if (window.scrollY > 80) {
-    document.querySelector(".header .header-2").classList.add("active");
+    document.querySelector("header .header-2").classList.add("active");
   } else {
-    document.querySelector(".header .header-2").classList.remove("active");
+    document.querySelector("header .header-2").classList.remove("active");
   }
 
   fadeOut();
